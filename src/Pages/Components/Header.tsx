6435d7b749f5fd07d6f2595876fdc95df412eaba
Mister-Gee <fakuadegbenga@gmail.com>
2023-04-26 03:05:44 +0100

    update

--- src/Pages/Components/Header.tsx
+++ src/Pages/Components/Header.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 
 const Header = () => {
+  const goToWhatsapp = () => {
+    window.open(
+      "https://api.whatsapp.com/send/?phone=231880538190&text=Tell%20me%20more%20about%20Monerce&type=phone_number&app_absent=0",
+      "_blank"
+    );
+  };
+
   return (
     <header>
       <div className="header-logo">
         <img className="desktop" src="assets/images/header-logo.png" alt="monerce" />
         <img className="mobile" src="assets/images/logo.svg" alt="monerce" />
       </div>
-      <div className="contact-whatsapp">
+      <div className="contact-whatsapp" onClick={goToWhatsapp}>
         <span>Can't Wait? Chat with us!</span>
         <img src="assets/images/whatsapp.png" alt="whatsapp" />
       </div>
